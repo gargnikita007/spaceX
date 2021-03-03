a4a5645bc5abba449d25e9de4ff1ba3c6102e983
unknown <you@example.com>
2021-03-03 14:22:12 +0530

    added path in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/spaceX'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/spaceX/'}),
+    res.sendFile('index.html', {root: 'dist/spaceX/index.html'}),
 );
 
 // Start the app by listening on the default Heroku port
