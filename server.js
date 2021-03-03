3fbf3268b5f29086414a5985e7cf1c4c0065d7b4
unknown <you@example.com>
2021-03-03 15:45:08 +0530

    change set file to index

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(express.static('./dist/spaceX'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/spaceX/'}),
+  //  res.sendFile('index.html', {root: 'dist/spaceX/'}),
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 );
 
 // Start the app by listening on the default Heroku port
