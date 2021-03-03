b34b5bbd74da9e4d927a8b165b3db8842dd303e4
Nikita Garg <47687306+gargnikita007@users.noreply.github.com>
2021-03-03 16:31:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/space-x'));
+app.use(express.static(__dirname + '/dist/space-x'));
 
-app.get('/*', (req, res) =>
-   res.sendFile('index.html', {root: 'dist/space-x/'}),
+app.get('/*', (req, res) => 
+        res.sendFile(path.join(__dirname+'/dist/space-x/index.html'));
+  // res.sendFile('index.html', {root: __dirname + 'dist/space-x/'}),
   
 );
 
